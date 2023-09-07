8a5e623248936ef118ce2f51ea4ef68b82fa0d3b
DIEGOPHRANCHESCO <phrandiego7@gmail.com>
2023-09-07 18:55:36 -0500

    deploy

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://diegophranchesco.github.io/pokemon/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://DIEGOPHRANCHESCO.github.io/deploypoke"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
